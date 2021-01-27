a29d9f53266be43f4ebddb4796d146a3a3b144fe
AndyLi23 <andyliqy@gmail.com>
2021-01-27 13:04:47 -0800

    working clock eyyyy

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,4 +82,64 @@ function goto(i) {
 }
 
 
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+var radius = canvas.height / 2;
+
+console.log(window.innerWidth);
+console.log(radius*2);
+
+if(window.innerWidth < radius*2) {
+    radius = window.innerWidth/2;
+} 
+
+console.log(radius*2);
+ctx.translate(radius, radius);
+radius = radius * 0.90
+drawClock(0, 0);
+
+function drawClock(hour, minute) {
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI, true);
+    ctx.fillStyle = "white";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(0, 0 ,radius * 0.95, 0, 2 * Math.PI);
+    ctx.fillStyle="#000";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.05, 0, 2 * Math.PI);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+
+    drawNumbers(ctx, radius);
+}
+
+
+
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.15 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for(num = 1; num < 13; num++){
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+}
+
+
+
+
+
 
